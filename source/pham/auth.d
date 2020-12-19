6d6730b417fe71d8865fa444b040cca668ef36fc
apz28 <apz28@hotmail.com>
2020-12-19 16:34:04 -0500

    Create auth.d

---
+++ source/pham/auth.d
@@ -0,0 +1,38 @@
+/*
+*
+* License: $(HTTP www.boost.org/LICENSE_1_0.txt, Boost License 1.0).
+* Authors: An Pham
+*
+* Copyright An Pham 2019 - xxxx.
+* Distributed under the Boost Software License, Version 1.0.
+* (See accompanying file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*
+*/
+
+module pham.db.auth;
+
+import pham.db.dbobject;
+
+nothrow @safe:
+
+abstract class DbAuth : DbDisposableObject
+{
+nothrow @safe:
+
+public:
+    ubyte[] getAuthData(const(char)[] userName, const(char)[] userPassword, ubyte[] serverAuthData);
+
+    const(ubyte)[] privateKey() const;
+    const(ubyte)[] publicKey() const;
+    const(ubyte)[] sessionKey() const;
+
+    version (unittest)
+    {
+        const(ubyte)[] serverPublicKey() const;
+        const(ubyte)[] serverSalt() const;
+    }
+
+    @property bool isSymantic() const;
+    @property string name() const;
+    @property string sessionKeyName() const;
+}
